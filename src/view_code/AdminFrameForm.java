25f90682a569702369e4f195fa41b9d28e401d84
Hai1576 <truongtuanhai2502@gmail.com>
2025-08-13 21:00:55 +0700

    Add AdminFrameForm.java and AdminFrameForm.form

---
+++ src/view_code/AdminFrameForm.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package view_code;
+
+/**
+ *
+ * @author Admin
+ */
+
+public class AdminFrameForm extends javax.swing.JFrame {
+
+    public AdminFrameForm() {
+        initComponents();
+        setLocationRelativeTo(null); // căn giữa
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButtonChuyenBay = new javax.swing.JButton();
+        jButtonVe = new javax.swing.JButton();
+        jButtonHanhKhach = new javax.swing.JButton();
+        jButtonMayBay = new javax.swing.JButton();
+        jButtonHang = new javax.swing.JButton();
+        jButtonTaiKhoan = new javax.swing.JButton();
+        jLabelSpace1 = new javax.swing.JLabel();
+        jButtonThoat = new javax.swing.JButton();
+        jLabelSpace2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Quản trị hệ thống - Admin");
+        setResizable(false);
+        getContentPane().setLayout(new java.awt.GridLayout(3, 3, 10, 10));
+
+        jButtonChuyenBay.setText("Quản lý chuyến bay");
+        getContentPane().add(jButtonChuyenBay);
+
+        jButtonVe.setText("Quản lý vé");
+        getContentPane().add(jButtonVe);
+
+        jButtonHanhKhach.setText("Quản lý hành khách");
+        getContentPane().add(jButtonHanhKhach);
+
+        jButtonMayBay.setText("Quản lý máy bay");
+        getContentPane().add(jButtonMayBay);
+
+        jButtonHang.setText("Quản lý hãng hàng không");
+        getContentPane().add(jButtonHang);
+
+        jButtonTaiKhoan.setText("Quản lý tài khoản");
+        getContentPane().add(jButtonTaiKhoan);
+
+        getContentPane().add(jLabelSpace1);
+
+        jButtonThoat.setText("Thoát");
+        jButtonThoat.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonThoatActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButtonThoat);
+
+        getContentPane().add(jLabelSpace2);
+
+        pack();
+        setSize(600, 400);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButtonThoatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonThoatActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jButtonThoatActionPerformed
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AdminFrameForm().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButtonChuyenBay;
+    private javax.swing.JButton jButtonHang;
+    private javax.swing.JButton jButtonHanhKhach;
+    private javax.swing.JButton jButtonMayBay;
+    private javax.swing.JButton jButtonTaiKhoan;
+    private javax.swing.JButton jButtonThoat;
+    private javax.swing.JButton jButtonVe;
+    private javax.swing.JLabel jLabelSpace1;
+    private javax.swing.JLabel jLabelSpace2;
+    // End of variables declaration//GEN-END:variables
+}
+
